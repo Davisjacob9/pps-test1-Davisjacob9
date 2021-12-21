40a49801fcc937c833b1484c6eb4fdc13b62bfab
Davis Jacob <93579312+Davisjacob9@users.noreply.github.com>
2021-12-21 04:08:53 +0000

    done

---
+++ p3original.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void input(int*a,int*b)
+{
+  printf("enter the values of a,b");
+  scanf("%d%d",a,b);
+}
+void add(int a,int b, int*sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("the sum of two numbers = %d+%d = %d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+  }
